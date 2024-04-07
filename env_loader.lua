5aec1f2c448e91963c7c4c8c7c499000815f22a9
Larry Kann <kanndide@gmail.com>
2024-04-07 11:45:47 -0400

    environment variable loader, because why not

---
+++ env_loader.lua
@@ -0,0 +1,21 @@
+-- env_loader.lua
+
+local M = {}
+
+function M.loadEnv(path)
+    local env = {}
+    for line in io.lines(path) do
+        local key, value = line:match("^([^=]+)=(.+)$")
+        if key and value then
+            -- Remove leading and trailing whitespace from key
+            key = key:match("^%s*(.-)%s*$")
+            -- Remove leading and trailing quotes from value
+            value = value:match('^%s*"(.-)"%s*$') or value:match("^%s*(.-)%s*$")
+            env[key] = value
+        end
+    end
+    return env
+end
+
+return M
+
