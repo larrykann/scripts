6fff86242822c380a4fbcc3e9410ce6dbda24e36
Larry Kann <kanndide@gmail.com>
2024-04-06 20:31:47 -0400

    env loader for lua

---
+++ env_loader.lua
@@ -0,0 +1,21 @@
+local function loadEnv(filePath)
+    local env = {}
+    local file, err = io.open(filePath, "r")
+    
+    if not file then
+        print("Could not open the .env file: " .. err)
+        return env
+    end
+    
+    for line in file:lines() do
+        local key, value = line:match("^([^=]+)=(.+)$")
+        if key and value then
+            env[key] = value
+        end
+    end
+    
+    file:close()
+    return env
+end
+
+return loadEnv
