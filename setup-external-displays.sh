7de793206a49dc2b8290f39570e5d34f234c8a59
Larry Kann <kanndide@gmail.com>
2024-04-10 09:00:35 -0400

    changed settings so laptop display stays on

--- setup-external-displays.sh
+++ setup-external-displays.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# External monitor ports
+# Your external monitor ports
 EXTERNAL_MONITOR_1="DP-1-0.1"
 EXTERNAL_MONITOR_2="DP-1-0.2"
 
-# Check if external monitors are connected
-if xrandr | grep "$EXTERNAL_MONITOR_1 connected" && xrandr | grep "$EXTERNAL_MONITOR_2 connected"; then
-    # External monitors are connected
+# Check if the first external monitor is connected
+if xrandr | grep "$EXTERNAL_MONITOR_1 connected"; then
     xrandr --output $EXTERNAL_MONITOR_1 --auto --right-of eDP
-    xrandr --output $EXTERNAL_MONITOR_2 --auto --right-of $EXTERNAL_MONITOR_1
-    xrandr --output eDP --off # Turn off laptop display
-else
-    # No external monitors found, enable laptop display
-    xrandr --output eDP --auto
 fi
+
+# Check if the second external monitor is connected
+if xrandr | grep "$EXTERNAL_MONITOR_2 connected"; then
+    # If both monitors are connected, place the second to the right of the first
+    if xrandr | grep "$EXTERNAL_MONITOR_1 connected"; then
+        xrandr --output $EXTERNAL_MONITOR_2 --auto --right-of $EXTERNAL_MONITOR_1
+    else
+        xrandr --output $EXTERNAL_MONITOR_2 --auto --right-of eDP
+    fi
+fi
+
+# Ensure the laptop display is always on
+xrandr --output eDP --auto
 
